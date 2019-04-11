ef8e34a7c1463a09120dbad268c8c833c59a70d6
virtual-shell-4609 <amandathedev@gmail.com>
2019-04-11 17:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let myArray = [];
+  for(var i = 0; i < musicians.length; i++) {
+    myArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return myArray;
+}
+
+function johnLennonFacts(facts) {
+  let myArray = [];
+  let i = 0;
+  while (i < facts.length) {
+    myArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return myArray;
+}
+
+function iLoveTheBeatles(num) {
+  let myArray = [];
+  let i = 0;
+  do {
+    myArray.push("I love the Beatles!" + i);
+    i++;
+  }
+  while (i < 15);
+  break
+  return myArray;
+}
